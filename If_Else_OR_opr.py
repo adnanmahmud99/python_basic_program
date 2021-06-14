1f8228d36c1205072e8d7945107d4e03306447cd
adnanmahmud99 <84793133+adnanmahmud99@users.noreply.github.com>
2021-06-14 21:05:38 +0600

    first commit

---
+++ If_Else_OR_opr.py
@@ -0,0 +1,11 @@
+gender = input("Are you  a male(y/n):")
+
+
+
+if gender == 'y' or gender == "Y":
+
+ print("Gender: Male")
+
+else:
+ print("Geder: Female")
+
---
+++ if_elif.py
@@ -0,0 +1,21 @@
+mark=int(input("Enter your mark:"))
+if mark<35:
+ print("Gerad F: Fail")
+
+elif mark<=35 and mark<=40:
+ print("Grade D")
+
+elif mark<=41 and mark<50:
+ print("Grade C")
+
+elif mark>=50 and mark<60:
+ print("Grade B")
+
+elif mark<=60 and mark<70:
+ print("Grade B+")
+
+elif mark>=70 and mark<80:
+ print("Grade D")
+
+else:
+ print("Grade A+")
