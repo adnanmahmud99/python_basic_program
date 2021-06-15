2b423cac32a5086bbf1bc450dc9ee6f4c444b864
adnanmahmud99 <84793133+adnanmahmud99@users.noreply.github.com>
2021-06-15 12:21:48 +0600

    1

---
+++ OOP in python/mysillyoblect.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(mysillyobject,name,age):
+      mysillyobject.name=name
+      mysillyobject.age=age
+
+    def myfunc(xyz):
+      print("hello my name is" +xyz.name)
+
+p1 = Person("Adnan Mahmud", 23)
+p1.myfunc()        
