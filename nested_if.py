dcc65765f7b4d52e70d18b60c35a27a6e6fb6d0e
adnanmahmud99 <84793133+adnanmahmud99@users.noreply.github.com>
2021-06-15 10:57:38 +0600

    done

---
+++ nested_if.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number: "))
+
+if n>10: 
+ print("the number is greater than to 10 ")
+if n % 2 == 0:
+ print("The number  is even");
+else:
+ print("The number is odd");
+ 
+ 
+ else:
+print("the number is smaller or equal to 10");
